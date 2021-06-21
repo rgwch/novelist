edfca3be853f18bf85524dfa321998b2ec5bb0ec
gerry <weirich@elexis.ch>
2021-06-21 17:32:32 +0200

    details

--- src/lib/services/store.spec.ts
+++ src/lib/services/store.spec.ts
@@ -4,7 +4,8 @@ describe("Store", () => {
   it("saves and retrieves serialized data", async () => {
     const store = new Store('default');
     const buffer = Buffer.alloc(100, "*", "utf-8")
-    expect(await store.save("test/dump.store", buffer)).toBeTruthy
+    const saved=await store.save("test/dump.store", buffer)
+    expect(saved).toBeTruthy
     const retrieved = await store.load("test/dump.store")
     expect(retrieved).toBeDefined
     expect(retrieved).toEqual(buffer)
--- src/lib/services/store.ts
+++ src/lib/services/store.ts
@@ -1,47 +1,85 @@
 import fs from 'fs'
 import { createGzip, createGunzip } from 'zlib';
 import crypto from 'crypto';
+import sb from 'stream-buffers'
+import { createModuleResolutionCache } from 'typescript';
 
 export class Store {
+  private algo = 'aes-192-cbc'
+  private key
 
-  constructor(private password: string) { }
-
-  private generateKey(password: string): any {
-    const algo = 'aes-192-cbc';
-    const key = crypto.scryptSync(password, 'salt', 24);
-    const iv = Buffer.alloc(16.0);
-    const cipher = crypto.createCipheriv(algo, key, iv);
-    const decipher = crypto.createDecipheriv(algo, key, iv);
-    return { cipher, decipher };
+  constructor(private password: string) {
+    this.key = crypto.scryptSync(this.password, 'salt', 24);
   }
 
   public save(id: string, data: Buffer): Promise<boolean> {
     return new Promise((resolve, reject) => {
+      const instream = new sb.ReadableStreamBuffer()
+      const iv = Buffer.alloc(16.0);
+      const cipher = crypto.createCipheriv(this.algo, this.key, iv);
       const gz = createGzip()
-      const output = fs.createWriteStream(id)
+      const outstream = fs.createWriteStream(id)
+      /*
       gz.pipe(output)
       gz.write(data)
       gz.end()
       resolve(true);
+      */
+
+      instream.on("error", err => {
+        console.log(err)
+        reject(err)
+      })
+      instream.on('end', () => {
+        gz.flush()
+        gz.end()
+        outstream.end()
+        resolve(true)
+      })
+      outstream.on("error", (err) => {
+        console.log(err)
+        reject(err)
+      })
+      /*
+      instream.on('data',chunk=>{
+       outstream.write(chunk)
+      })
+      */
+      instream.put(data)
+      instream.stop()
+      instream.pipe(gz).pipe(outstream)
+      // instream.pipe(outstream)
     })
 
   }
 
   public load(id: string): Promise<Buffer> {
     return new Promise((resolve, reject) => {
-      const input = fs.createReadStream(id)
+      const iv = Buffer.alloc(16.0);
+      const decipher = crypto.createDecipheriv(this.algo, this.key, iv);
       const gz = createGunzip()
-      const chunks = []
-      let buffer
-      input.pipe(gz)
-      gz.on('data', chunk => chunks.push(chunk))
+      const instream = fs.createReadStream(id)
+      const outstream = new sb.WritableStreamBuffer({
+        initialSize: 1024,
+        incrementAmount: 1024
+      })
+      instream.on('error', (err) => {
+        console.log("Instream error " + err)
+        reject(null)
+      })
+      gz.on('error', err => {
+        console.log("gzip error " + err)
+        reject(null)
+      })
       gz.on('end', () => {
-        buffer = Buffer.concat(chunks)
-        resolve(buffer)
+        console.log(outstream.size())
+        const buf = outstream.getContents()
+        if (!buf) {
+          reject(null)
+        }
+        resolve(buf)
       })
-      gz.on("error", err => {
-        reject(err)
-      })
+      instream.pipe(gz).pipe(outstream)
     })
 
   }
