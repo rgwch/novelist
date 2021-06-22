cea005d13058fdfa6aeeb89fb4f6046002ddcb88
rgwch <rgw@elexis.ch>
2021-06-22 07:06:52 +0200

    fixed test

--- src/lib/services/novel.ts
+++ src/lib/services/novel.ts
@@ -3,218 +3,219 @@ import path from 'path';
 import YAML from 'yaml';
 import metadataParser from 'markdown-yaml-metadata-parser';
 import { Store } from './store';
-import {promisify} from 'util'
+import { promisify } from 'util'
 
-const preadFile=promisify(fs.readFile)
-const preaddir=promisify(fs.readdir)
+const preadFile = promisify(fs.readFile)
+const preaddir = promisify(fs.readdir)
 
 type metadata_def = {
-	title: string;
-	author?: string;
-	created: Date;
-	modified?: Date;
-	chapters: Array<string>;
-	persons: Array<string>;
-	places: Array<string>;
-	// [propName: string]: any;
+  title: string;
+  author?: string;
+  created: Date;
+  modified?: Date;
+  chapters: Array<string>;
+  persons: Array<string>;
+  places: Array<string>;
+  // [propName: string]: any;
 };
 const default_metadata: metadata_def = {
-	title: '',
-	author: '',
-	created: new Date(),
-	chapters: [],
-	persons: [],
-	places: []
+  title: '',
+  author: '',
+  created: new Date(),
+  chapters: [],
+  persons: [],
+  places: []
 };
 type person_def = {
-	name: string;
-	nicknames?: Array<string>;
-	gender?: 'm' | 'f';
-	height?: number;
-	birthDate?: Date;
-	description?: string;
+  name: string;
+  nicknames?: Array<string>;
+  gender?: 'm' | 'f';
+  height?: number;
+  birthDate?: Date;
+  description?: string;
 };
 type place_def = {
-	name: string;
-	surround?: string;
-	description?: string;
+  name: string;
+  surround?: string;
+  description?: string;
 };
 type chapter_def = {
-	title: string;
-	persons?: Array<string>;
-	places?: Array<string>;
-	summary?: string;
-	time?: string;
-	text?: string;
+  title: string;
+  persons?: Array<string>;
+  places?: Array<string>;
+  summary?: string;
+  time?: string;
+  text?: string;
 };
 type noveldef = {
-	metadata: metadata_def;
-	expose?: string;
-	persons?: {
-		[name: string]: person_def;
-	};
-	places?: {
-		[name: string]: place_def;
-	};
-	timeline?: string;
-	chapters: {
-		[name: string]: chapter_def;
-	};
+  metadata: metadata_def;
+  expose?: string;
+  persons?: {
+    [name: string]: person_def;
+  };
+  places?: {
+    [name: string]: place_def;
+  };
+  timeline?: string;
+  chapters: {
+    [name: string]: chapter_def;
+  };
 };
 
 
 
 export class Novel {
-	private store: Store;
-	static open(pathname: string, password = 'default'): Promise<Novel> {
-		console.log("opening "+pathname+" from "+__dirname)
-		return new Promise((resolve, reject) => {
-			if (fs.existsSync(pathname)) {
-				const store = new Store(password);
-				store.load(pathname).then((buffer) => {
-					resolve(new Novel(pathname, JSON.parse(buffer.toString('utf-8'))));
-				});
-			} else {
-				const def = {
-					metadata: default_metadata,
-					persons: {},
-					places: {},
-					chapters: {},
-					time: ''
-				};
-				def.metadata.created = new Date();
-				const novel=new Novel(pathname,def)
-				novel.flush()
-				resolve(novel);
-			}
-		});
-	}
+  private store: Store;
+  static open(pathname: string, password = 'default'): Promise<Novel> {
+    // console.log("opening " + pathname + " from " + __dirname)
+    return new Promise((resolve, reject) => {
+      if (fs.existsSync(pathname)) {
+        const store = new Store(password);
+        store.load(pathname).then((buffer) => {
+          resolve(new Novel(pathname, JSON.parse(buffer.toString('utf-8'))));
+        });
+      } else {
+        const def = {
+          metadata: default_metadata,
+          persons: {},
+          places: {},
+          chapters: {},
+          time: ''
+        };
+        def.metadata.created = new Date();
+        const novel = new Novel(pathname, def)
+        novel.flush().then(() => {
+          resolve(novel);
+        })
+      }
+    });
+  }
 
-	static async fromDirectory(dir: string, password?: string): Promise<Novel> {
-		const def: noveldef = {
-			metadata: default_metadata,
-			persons: {},
-			places: {},
-			chapters: {},
-			timeline: ''
-		};
-		def.metadata.created = new Date();
-		try {
-			const time = await preadFile(path.resolve(dir, 'time.md'));
-			def.timeline = time.toString('utf-8');
-		} catch (err) {
-			console.log('no time def found ' + err);
-			def.timeline = '';
-		}
-		try {
-			const meta = await preadFile(path.resolve(dir, 'metadata.yaml'));
-			def.metadata = YAML.parse((await meta).toString('utf-8'));
-		} catch (err) {
-			console.log('no metadata found ' + err);
-			def.metadata = default_metadata;
-		}
-		try {
-			const persons = await preaddir(path.resolve(dir, 'persons'));
-			for (const person of persons) {
-				const data = await preadFile(path.resolve(dir, 'persons', person));
-				const split = metadataParser(data.toString('utf-8'));
-				def.persons[split.metadata.name] = split.metadata;
-				def.persons[split.metadata.name].description = split.content;
-			}
-		} catch (err) {
-			console.log('no persons ' + err);
-		}
-		try {
-			const places = await preaddir(path.resolve(dir, 'places'));
-			for (const place of places) {
-				const data = await preadFile(path.resolve(dir, 'places', place));
-				const split = metadataParser(data.toString('utf-8'));
-				def.places[split.metadata.name] = split.metadata;
-				def.places[split.metadata.name].description = split.content;
-			}
-		} catch (err) {
-			console.log('no places ' + err);
-		}
-		try {
-			const chapters = await preaddir(path.resolve(dir, 'chapters'));
-			for (const chapter of chapters) {
-				const data = await preadFile(path.resolve(dir, 'chapters', chapter));
-				const split = metadataParser(data.toString('utf-8'));
-				def.chapters[split.metadata.title] = split.metadata;
-				def.chapters[split.metadata.title].text = split.content;
-			}
-		} catch (err) {
-			console.log('no chapters');
-		}
+  static async fromDirectory(dir: string, password?: string): Promise<Novel> {
+    const def: noveldef = {
+      metadata: default_metadata,
+      persons: {},
+      places: {},
+      chapters: {},
+      timeline: ''
+    };
+    def.metadata.created = new Date();
+    try {
+      const time = await preadFile(path.resolve(dir, 'time.md'));
+      def.timeline = time.toString('utf-8');
+    } catch (err) {
+      console.log('no time def found ' + err);
+      def.timeline = '';
+    }
+    try {
+      const meta = await preadFile(path.resolve(dir, 'metadata.yaml'));
+      def.metadata = YAML.parse((await meta).toString('utf-8'));
+    } catch (err) {
+      console.log('no metadata found ' + err);
+      def.metadata = default_metadata;
+    }
+    try {
+      const persons = await preaddir(path.resolve(dir, 'persons'));
+      for (const person of persons) {
+        const data = await preadFile(path.resolve(dir, 'persons', person));
+        const split = metadataParser(data.toString('utf-8'));
+        def.persons[split.metadata.name] = split.metadata;
+        def.persons[split.metadata.name].description = split.content;
+      }
+    } catch (err) {
+      console.log('no persons ' + err);
+    }
+    try {
+      const places = await preaddir(path.resolve(dir, 'places'));
+      for (const place of places) {
+        const data = await preadFile(path.resolve(dir, 'places', place));
+        const split = metadataParser(data.toString('utf-8'));
+        def.places[split.metadata.name] = split.metadata;
+        def.places[split.metadata.name].description = split.content;
+      }
+    } catch (err) {
+      console.log('no places ' + err);
+    }
+    try {
+      const chapters = await preaddir(path.resolve(dir, 'chapters'));
+      for (const chapter of chapters) {
+        const data = await preadFile(path.resolve(dir, 'chapters', chapter));
+        const split = metadataParser(data.toString('utf-8'));
+        def.chapters[split.metadata.title] = split.metadata;
+        def.chapters[split.metadata.title].text = split.content;
+      }
+    } catch (err) {
+      console.log('no chapters');
+    }
 
-		const novel = new Novel(path.resolve(dir, '../..', `${dir}.novel`), def, password);
-		await novel.flush();
-		return novel;
-	}
+    const novel = new Novel(path.resolve(dir, '../..', `${dir}.novel`), def, password);
+    await novel.flush();
+    return novel;
+  }
 
-	constructor(private pathname: string, private def: noveldef, private password = 'default') {
-		this.store = new Store(this.password);
-	}
+  constructor(private pathname: string, private def: noveldef, private password = 'default') {
+    this.store = new Store(this.password);
+  }
 
-	flush(): Promise<boolean> {
-		this.def.metadata.modified = new Date();
-		const buff = Buffer.from(JSON.stringify(this.def));
-		return this.store.save(this.pathname, buff);
-	}
+  flush(): Promise<boolean> {
+    this.def.metadata.modified = new Date();
+    const buff = Buffer.from(JSON.stringify(this.def));
+    return this.store.save(this.pathname, buff);
+  }
 
-	writeExpose(text: string): void {
-		this.def.expose = text;
-		this.flush();
-	}
+  writeExpose(text: string): void {
+    this.def.expose = text;
+    this.flush();
+  }
 
-	writePerson(pdef: person_def): void {
-		const name = pdef.name;
-		this.def.persons[name] = pdef;
-		if (!this.def.metadata.persons.find((p) => p == name)) {
-			this.def.metadata.persons.push(name);
-		}
-		this.flush();
-	}
-	writeChapter(cdef: chapter_def): void {
-		const title = cdef.title;
-		this.def.chapters[title] = cdef;
-		if (!this.def.metadata.chapters.find((c) => c == title)) {
-			this.def.metadata.chapters.push(title);
-		}
-		this.flush();
-	}
-	writePlace(pdef: place_def): void {
-		const name = pdef.name;
-		this.def.places[name] = pdef;
-		if (!this.def.metadata.places.find((p) => p == name)) {
-			this.def.metadata.places.push(name);
-		}
-		this.flush();
-	}
-	getPerson(name: string): person_def {
-		return this.def.persons[name];
-	}
-	getChapter(title: string): chapter_def {
-		return this.def.chapters[title];
-	}
-	getPlace(name: string): place_def {
-		return this.def.places[name];
-	}
+  writePerson(pdef: person_def): void {
+    const name = pdef.name;
+    this.def.persons[name] = pdef;
+    if (!this.def.metadata.persons.find((p) => p == name)) {
+      this.def.metadata.persons.push(name);
+    }
+    this.flush();
+  }
+  writeChapter(cdef: chapter_def): void {
+    const title = cdef.title;
+    this.def.chapters[title] = cdef;
+    if (!this.def.metadata.chapters.find((c) => c == title)) {
+      this.def.metadata.chapters.push(title);
+    }
+    this.flush();
+  }
+  writePlace(pdef: place_def): void {
+    const name = pdef.name;
+    this.def.places[name] = pdef;
+    if (!this.def.metadata.places.find((p) => p == name)) {
+      this.def.metadata.places.push(name);
+    }
+    this.flush();
+  }
+  getPerson(name: string): person_def {
+    return this.def.persons[name];
+  }
+  getChapter(title: string): chapter_def {
+    return this.def.chapters[title];
+  }
+  getPlace(name: string): place_def {
+    return this.def.places[name];
+  }
 
-	getExpose(): string {
-		return this.def.expose;
-	}
-	readMetadata(): metadata_def {
-		return this.def.metadata;
-	}
-	writeMetadata(meta: metadata_def): void {
-		this.def.metadata = meta;
-	}
-	getTimeline(): string {
-		return this.def.timeline;
-	}
-	addTimestamp(ds: string): void {
-		const t = this.def.timeline + ds;
-		this.def.timeline = t;
-	}
+  getExpose(): string {
+    return this.def.expose;
+  }
+  readMetadata(): metadata_def {
+    return this.def.metadata;
+  }
+  writeMetadata(meta: metadata_def): void {
+    this.def.metadata = meta;
+  }
+  getTimeline(): string {
+    return this.def.timeline;
+  }
+  addTimestamp(ds: string): void {
+    const t = this.def.timeline + ds;
+    this.def.timeline = t;
+  }
 }
--- src/lib/services/store.spec.ts
+++ src/lib/services/store.spec.ts
@@ -3,15 +3,15 @@ import fs from 'fs'
 
 
 describe("Store", () => {
-  afterAll(()=>{
+  afterAll(() => {
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    fs.rm("test/dump.store",()=>{})
+    fs.rm("test/dump.store", () => { })
   })
 
   it("saves and retrieves serialized data", async () => {
     const store = new Store('default');
     const buffer = Buffer.alloc(100, "*", "utf-8")
-    const saved=await store.save("test/dump.store", buffer)
+    const saved = await store.save("test/dump.store", buffer)
     expect(saved).toBeTruthy
     const retrieved = await store.load("test/dump.store")
     expect(retrieved).toBeDefined
