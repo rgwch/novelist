bc0dc94a8243f210463ad87154b504e49b038e95
gerry <weirich@elexis.ch>
2021-06-22 18:21:00 +0200

    detail

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,24 +2,25 @@
 	import { onMount } from 'svelte';
 	import type { metadata_def } from '$lib/services/novel.d';
 	import '../../node_modules/simplemde/dist/simplemde.min.css';
-  import {Translator, trl} from '$lib/services/i18n'
-	let metadata
+	import { Translator, trl } from '$lib/services/i18n';
+	let metadata;
 	let container;
 	let editor;
 
-  const translator = new Translator();
-  
+	const translator = new Translator();
 
 	onMount(async () => {
-    await translator.init("de");
+		await translator.init('de');
 		const res = await fetch('/novel/metadata.json');
 		if (res.ok) {
 			const md = await res.json();
 			metadata = md.metadata;
 		}
-		const module = await import('simplemde');
-		const SimpleMDE = module.default;
-		editor = new SimpleMDE({ element: container });
+		if (!editor) {
+			let module = await import('simplemde');
+			let SimpleMDE = module.default;
+			editor = new SimpleMDE({ element: container });
+		}
 	});
 
 	let bookname;
@@ -30,7 +31,7 @@
 			const result = await res.json();
 			if (result.result !== 'fail') {
 				metadata = result.result;
-        console.log("metadata="+metadata)
+				console.log('metadata=' + metadata);
 			}
 		}
 	}
@@ -39,11 +40,13 @@
 <template>
 	{#if metadata}
 		<p>{JSON.stringify(metadata)}</p>
-		<h1 class="h1">{metadata.title}, {trl("general.created")}: {new Date(metadata.created).toString()}</h1>
+		<h1 class="h1">
+			{metadata.title}, {trl('general.created')}: {new Date(metadata.created).toString()}
+		</h1>
 	{:else}
 		<h1>Open Book</h1>
-		<input type="text" id="name" bind:this={bookname} />
-		<button on:click={openbook}>{trl("general.open")}</button>
+		<input class="border" type="text" id="name" bind:this={bookname} />
+		<button class="bg-green-300" on:click={openbook}>{trl('general.open')}</button>
 	{/if}
-  <textarea id=".editor" bind:this={container} />
+	<textarea id=".editor" bind:this={container} />
 </template>
