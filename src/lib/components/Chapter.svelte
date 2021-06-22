9812c82336ae9dae8fa6c34a166772cd677e5ef6
rgwch <weirich@elexis.ch>
2021-06-22 21:20:16 +0200

    svelte-i18n

--- src/lib/components/Chapter.svelte
+++ src/lib/components/Chapter.svelte
@@ -1,12 +1,35 @@
 <script lang="ts">
-  
+	import { onMount } from 'svelte';
+    export let metadata
+	let container;
+	let editor;
+
+	onMount(async () => {
+		let module = await import('simplemde');
+		let SimpleMDE = module.default;
+		editor = new SimpleMDE({ element: container });
+	});
 </script>
 
 <template>
-
+	<div class="flex gap-4 flex-row">
+		<div class="flex-none h-full">
+            <p>Kapitel</p>
+			<ul>
+				{#if metadata}
+                {#each metadata.chapters as chapter}
+                   <ul>{chapter}</ul> 
+                {/each}
+				{:else}
+				<ul>Neu...</ul>
+				{/if}
+			</ul>
+		</div>
+		<div class="flex-1 h-full">
+			<textarea bind:this={container} />
+		</div>
+	</div>
 </template>
 
 <style lang="scss">
- 
-  
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,26 +1,38 @@
+
 <script lang="ts">
+	import '$lib/services/i18n/i18n';
+	
 	import { onMount } from 'svelte';
 	import type { metadata_def } from '$lib/services/novel.d';
 	import '../../node_modules/simplemde/dist/simplemde.min.css';
-	import { Translator, trl } from '$lib/services/i18n';
+	import Chapter from '$lib/components/Chapter.svelte';
+	import { _ } from 'svelte-i18n';
+	
+	
 	let metadata;
-	let container;
-	let editor;
 
-	const translator = new Translator();
+	const visible = {
+		chapter: 'invisible',
+		persons: 'invisible',
+		places: 'invisible'
+	};
 
+	function toggle(elem) {
+		visible[elem] = visible[elem] === 'visible' ? 'invisible' : 'visible';
+	}
 	onMount(async () => {
-		await translator.init('de');
 		const res = await fetch('/novel/metadata.json');
 		if (res.ok) {
 			const md = await res.json();
 			metadata = md.metadata;
 		}
+		/*
 		if (!editor) {
 			let module = await import('simplemde');
 			let SimpleMDE = module.default;
-			editor = new SimpleMDE({ element: container });
+			editor = new SimpleMDE({ element: ed_chapter });
 		}
+		*/
 	});
 
 	let bookname;
@@ -41,12 +53,21 @@
 	{#if metadata}
 		<p>{JSON.stringify(metadata)}</p>
 		<h1 class="h1">
-			{metadata.title}, {trl('general.created')}: {new Date(metadata.created).toString()}
+			{metadata.title}, {$_('general.created')}: {new Date(metadata.created).toString()}
 		</h1>
 	{:else}
 		<h1>Open Book</h1>
 		<input class="border" type="text" id="name" bind:this={bookname} />
-		<button class="bg-green-300" on:click={openbook}>{trl('general.open')}</button>
+		<button class="bg-green-300" on:click={openbook}>{$_('general.open')}</button>
 	{/if}
-	<textarea id=".editor" bind:this={container} />
+	<br />
+	<span
+		role="button"
+		on:click={() => {
+			toggle('chapter');
+		}}>Editor</span
+	>
+	<div class={visible.chapter}>
+		<Chapter {metadata} />
+	</div>
 </template>
