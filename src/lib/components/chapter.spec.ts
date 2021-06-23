c071bf36ea9ba199ee0d9603444091e10fa4c6df
gerry <weirich@elexis.ch>
2021-06-23 10:42:19 +0200

    fix test

--- src/lib/components/chapter.spec.ts
+++ src/lib/components/chapter.spec.ts
@@ -4,6 +4,7 @@
 
 import Chapter from './Chapter.svelte'
 import { render } from '@testing-library/svelte';
+import '../services/i18n/i18n'
 
 describe('Chapter Component', () => {
   it('Should create', () => {
