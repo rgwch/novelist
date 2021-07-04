dd7e2afd18d9735dcd91b078c405ae2fe0318589
rgwch <rgw@elexis.ch>
2021-07-04 06:48:17 +0200

    make config optional

--- src/lib/global.ts
+++ src/lib/global.ts
@@ -1,17 +1,17 @@
 import fs from 'fs'
 import os from 'os'
 import config from './config.json'
-export default{
-    novel:null,
-    
-    resolveDir: () => {
-        let basedir=os.homedir()
-        if(process.env.NOVELS_DIR && fs.existsSync(process.env.NOVELS_DIR)){
-            basedir=process.env.NOVELS_DIR
-        }
-        if(config.basedir && fs.existsSync(config.basedir)){
-            basedir=config.basedir
-        }
-        return basedir
+export default {
+  novel: null,
+
+  resolveDir: () => {
+    let basedir = os.homedir()
+    if (process.env.NOVELS_DIR && fs.existsSync(process.env.NOVELS_DIR)) {
+      basedir = process.env.NOVELS_DIR
     }
+    if (config['basedir'] && fs.existsSync(config['basedir'])) {
+      basedir = config['basedir']
+    }
+    return basedir
+  }
 }
