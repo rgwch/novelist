c656457c3d17c64b721536ed98e44014272205ec
rgwch <rgw@elexis.ch>
2021-07-14 18:51:07 +0200

    storybook ok

--- client/.config/storybook/main.js
+++ client/.config/storybook/main.js
@@ -6,9 +6,32 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-svelte-csf"
+    "@storybook/addon-svelte-csf",
+    "@storybook/addon-postcss"
   ],
   "svelteOptions": {
     "preprocess": require("../../svelte.config.js").preprocess
+  },
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [
+                require('autoprefixer'),
+                require('tailwindcss')
+              ],
+              include: "../../src"
+            }
+          }
+        }
+      ]
+    });
+
+    return config;
   }
+
 }
