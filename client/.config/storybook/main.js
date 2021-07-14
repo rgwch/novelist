6a9e003f0d7f5ecb61254fa302340b0299823daa
rgwch <weirich@elexis.ch>
2021-07-14 19:02:26 +0200

    stobo

--- client/.config/storybook/main.js
+++ client/.config/storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   "stories": [
     "../../src/**/*.stories.mdx",
@@ -7,8 +9,29 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-svelte-csf"
+    /* "@storybook/addon-postcss" */
   ],
   "svelteOptions": {
     "preprocess": require("../../svelte.config.js").preprocess
-  }
+  } /*,
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        { loader: "style-loader" },
+        { loader: "css-loader", options: { modules: true, importLoaders: 1 } },
+        {
+          loader: 'postcss-loader',
+          options: {
+            ident: 'postcss',
+            plugins: [
+              require('tailwindcss')
+            ],
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../../'),
+    })
+    return config
+  } */
 }
