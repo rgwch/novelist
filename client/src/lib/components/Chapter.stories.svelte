a4a82002236d16f83cf903c1ad12f4a8e492e51b
rgwch <weirich@elexis.ch>
2021-07-14 15:59:45 +0200

    added storybook

---
+++ client/src/lib/components/Chapter.stories.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import '../services/i18n/i18n';
+	import Chapter from './Chapter.svelte';
+</script>
+
+<Meta title="Chapter" component={Chapter} />
+
+<Template let:args>
+	<Chapter {...args} />
+</Template>
+
+<Story name="Primary" args={{ meta: {} }} />
---
+++ client/src/lib/components/Dropdown.stories.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import '../services/i18n/i18n';
+	import Dropdown from './Dropdown.svelte';
+	const definition = {
+		type: 'dings',
+		newelem: 'new dings',
+		promptname: 'new dings'
+	};
+	const meta = {
+		dings: ['one', 'two', ' more']
+	};
+</script>
+
+<Meta title="Dropdown" component={Dropdown} />
+
+<Template let:args>
+	<Dropdown {...args} />
+</Template>
+
+<Story name="Default" args={{ metadata: meta, definition }} />
