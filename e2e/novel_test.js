c067921ef5863caa36c9ddce2f1443e0605d03c4
gerry <weirich@elexis.ch>
2021-07-19 14:31:50 +0200

    first e2etest

--- e2e/novel_test.js
+++ e2e/novel_test.js
@@ -1,19 +1,11 @@
 Feature('novel');
 
-Scenario('test something', ({ I }) => {
+Scenario('test create book', ({ I }) => {
   I.amOnPage("/")
   I.seeElement({ css: '#name' })
   I.fillField({ css: '#name' }, "e2etest")
-  I.amAcceptingPopups();
-
-  I.wait(2)
   I.click(".btn")
-  I.wait(2)
   I.type("test")
-  I.wait(2)
-  I.acceptPopup()
-
-  //I.click("OK")
-  I.wait(2)
+  I.click("OK")
 
 });
