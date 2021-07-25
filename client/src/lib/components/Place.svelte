ed56179123c1785f3b63a23451b41a59c900c173
rgwch <rgw@elexis.ch>
2021-07-25 13:04:45 +0200

    cleanup

--- client/src/lib/components/Place.svelte
+++ client/src/lib/components/Place.svelte
@@ -63,15 +63,6 @@
 </script>
 
 <template>
-	<div class="flex gap-4 flex-col">
-		<div class="flex-none h-full">
-			<Dropdown {metadata} {definition} on:selected={select} />
-		</div>
-
-		<div class="flex-1 h-full">
-			<div class="flex flex-row">
-				<Fieldeditor {fields} entity={currentPlace} on:save={saveFields} on:delete={del} />
-			</div>
-		</div>
-	</div>
+	<Dropdown {metadata} {definition} on:selected={select} />
+	<Fieldeditor {fields} entity={currentPlace} on:save={saveFields} on:delete={del} />
 </template>
