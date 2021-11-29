09465264557540d34d4eed2b5b3f52fde01b184c
rgwch <rgw@elexis.ch>
2021-11-29 21:03:05 +0100

    cryt

--- server/src/store.ts
+++ server/src/store.ts
@@ -3,29 +3,28 @@
  * License and Terms see LICENSE            *
  ********************************************/
 
-import fs from 'fs';
-import path from 'path';
-import { createGzip, createGunzip } from 'zlib';
-import crypto from 'crypto';
-import sb from 'stream-buffers';
-import { pipeline } from 'stream';
-import { DateTime } from 'luxon';
+import fs from 'fs'
+import path from 'path'
+import sb from 'stream-buffers'
+import { DateTime } from 'luxon'
 import os from 'os'
 import config from 'config'
+import { Crypter } from './crypt'
+import { pipeline } from 'stream'
 
-let plaintext = false;
+let plaintext = false
 
-if(config.has('encryption')){
-  plaintext=!config.get('encryption')
+if (config.has('encryption')) {
+  plaintext = !config.get('encryption')
 }
 
 export function resolveDir() {
-  let ret:string = os.homedir()
+  let ret: string = os.homedir()
   if (process.env.NOVELS_DIR && fs.existsSync(process.env.NOVELS_DIR)) {
     ret = process.env.NOVELS_DIR
   }
-  if (config.has("basedir")) {
-    const basedir:string = config.get("basedir")
+  if (config.has('basedir')) {
+    const basedir: string = config.get('basedir')
     if (basedir && fs.existsSync(basedir)) {
       ret = basedir
     }
@@ -33,145 +32,95 @@ export function resolveDir() {
   return ret
 }
 export function setPlaintext(plain: boolean): void {
-  plaintext = plain;
+  plaintext = plain
 }
 export class Store {
-  private algo = 'aes-192-cbc';
-  private key: any;
-  private iv: Buffer = Buffer.alloc(16, 0);
+  private crypter: Crypter
 
   constructor(passphrase: string) {
-    this.setPassword(passphrase);
+    const salt = config.has('salt') ? config.get('salt') : 'someSalt'
+    this.crypter = new Crypter(passphrase, salt.toString())
   }
 
-  setPassword(passphrase: string): void {
-    // console.log("Store: Setting password " + password)
-    const hash = crypto.createHash('sha256')
-    const pwd = hash.update(passphrase)
-    const salt:string = config.has("salt") ? config.get("salt") : "someSalt"
-    this.key = crypto.scryptSync(passphrase, salt, 24);
-
-    const ivraw = pwd.digest();
-    this.iv = Buffer.from(ivraw.subarray(0, 16));
-  }
   performBackup(gen: number, pathname: string): void {
-    const last = pathname + '_' + gen;
+    const last = pathname + '_' + gen
     if (fs.existsSync(last)) {
-      const now = DateTime.fromJSDate(new Date());
-      const datestring = now.toFormat('yyyy-LL-dd');
-      const basename = path.basename(pathname, '.novel');
-      const dir = path.dirname(pathname);
-      const dailybackup = path.join(dir, basename + '_' + datestring + '.novel');
+      const now = DateTime.fromJSDate(new Date())
+      const datestring = now.toFormat('yyyy-LL-dd')
+      const basename = path.basename(pathname, '.novel')
+      const dir = path.dirname(pathname)
+      const dailybackup = path.join(dir, basename + '_' + datestring + '.novel')
       if (fs.existsSync(dailybackup)) {
-        fs.rmSync(last);
+        fs.rmSync(last)
       } else {
-        fs.renameSync(last, dailybackup);
+        fs.renameSync(last, dailybackup)
       }
     }
     for (let i = gen - 1; i > 0; i--) {
-      const check = pathname + '_' + i;
+      const check = pathname + '_' + i
       if (fs.existsSync(check)) {
-        fs.renameSync(check, pathname + '_' + (i + 1).toString());
+        fs.renameSync(check, pathname + '_' + (i + 1).toString())
       }
     }
     if (fs.existsSync(pathname)) {
-      fs.renameSync(pathname, pathname + '_1');
+      fs.renameSync(pathname, pathname + '_1')
     }
   }
 
   public save(id: string, data: Buffer): Promise<boolean> {
     return new Promise((resolve, reject) => {
       try {
-        this.performBackup(5, id);
+        this.performBackup(5, id)
       } catch (err) {
-        console.log("store:save: can't organise backups " + err);
-        reject('Backup ' + err);
+        console.log("store:save: can't organise backups " + err)
+        reject('Backup ' + err)
       }
 
-      const instream = new sb.ReadableStreamBuffer();
-      // const iv = Buffer.alloc(16, 0);
-      const cipher = crypto.createCipheriv(this.algo, this.key, this.iv);
-      const gz = createGzip();
-      try {
-        const outstream = fs.createWriteStream(id);
-
-        instream.on('error', (err) => {
-          reject('instream ' + err);
-        });
-        outstream.on('error', (err) => {
-          reject('outstream ' + err);
-        });
-        instream.put(data);
-        instream.stop();
-        if (plaintext) {
-          pipeline(instream, outstream, (err) => {
-            if (err) {
-              reject('pipeline ' + err);
-            } else {
-              outstream.end();
-              resolve(true);
-            }
-          });
-        } else {
-          pipeline(instream, gz, cipher, outstream, (err) => {
-            if (err) {
-              reject('pipeline ' + err);
-            } else {
-              outstream.end();
-              resolve(true);
-            }
-          });
-        }
-      } catch (err) {
-        console.log('store: save error ' + err);
-        reject(err);
+      const instream = new sb.ReadableStreamBuffer()
+      const outstream = fs.createWriteStream(id)
+      instream.put(data)
+      instream.stop()
+      if (plaintext) {
+        pipeline(instream, outstream, (err) => {
+          if (err) {
+            reject('pipeline ' + err)
+          } else {
+            outstream.end()
+            resolve(true)
+          }
+        })
+      } else {
+        return this.crypter.encrypt(instream, outstream)
       }
-    });
+    })
   }
 
   public load(id: string): Promise<Buffer> {
     return new Promise((resolve, reject) => {
-      // const iv = Buffer.alloc(16, 0);
-      const decipher = crypto.createDecipheriv(this.algo, this.key, this.iv);
-      const gz = createGunzip();
-      const instream = fs.createReadStream(id);
+      const instream = fs.createReadStream(id)
       const outstream = new sb.WritableStreamBuffer({
         initialSize: 1024,
-        incrementAmount: 1024
-      });
-      instream.on('error', (err) => {
-        console.log('store.load: Instream error ' + err);
-        reject('instream ' + err);
-      });
-      gz.on('error', (err) => {
-        console.log('store.load: gzip error ' + err);
-        reject('gzip ' + err);
-      });
+        incrementAmount: 1024,
+      })
       if (plaintext) {
         pipeline(instream, outstream, (err) => {
           if (err) {
-            reject('pipeline ' + err);
+            reject('pipeline ' + err)
           } else {
-            const buf = outstream.getContents();
+            const buf = outstream.getContents()
             if (!buf) {
-              reject('empty buffer');
+              reject('empty buffer')
             }
-            resolve(buf as Buffer);
+            resolve(buf as Buffer)
           }
-        });
+        })
       } else {
-        pipeline(instream, decipher, gz, outstream, (err) => {
-          if (err) {
-            reject('pipeline ' + err);
-          } else {
-            const buf = outstream.getContents();
-            if (!buf) {
-              reject('empty buffer');
-            }
-            resolve(buf as Buffer);
+        return this.crypter.decrypt(instream, outstream).then((buf) => {
+          if (buf) {
+            return outstream.getContents()
           }
-        });
+        })
       }
-    });
+    })
   }
 }
