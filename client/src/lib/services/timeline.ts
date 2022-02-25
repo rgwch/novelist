9a511fedf5cc859d89f42958bb6477c10baf0a52
rgwch <weirich@elexis.ch>
2022-02-25 07:14:35 +0100

    detais

--- client/src/lib/services/timeline.ts
+++ client/src/lib/services/timeline.ts
@@ -1,6 +1,34 @@
-import {load,save} from './fileio'
-export class Times{
-  constructor(){
+import { load, save } from './fileio'
+export type TimelineEntry = {
+  chapter: string   // Chapter this entry belongs to
+  date: string      // Date/time for this entry as YYYY-MM-DDTHH:MM:SS
+  offset: number    // Offset of this entry from the beginning  in units
+  unit: "seconds" | "minutes" | "hours" | "days" | "weeks" | "months" | "years"
+}
+export class Times {
+  private entries: Array<TimelineEntry> = []
+
+  private async scan() {
+    try {
+      const raw = await load('timeline', "")
+      this.entries = JSON.parse(raw)
+    } catch (err) {
+      alert(err)
+    }
+  }
+
+  public async fetch(): Promise<Array<TimelineEntry>> {
+    await this.scan()
+    return this.entries
+  }
+
+  public async save() {
+    const proc = JSON.stringify(this.entries)
+    await save('timeline', proc)
+  }
+
+  public add(point: string, chapter: string) {
     
+
   }
 }
